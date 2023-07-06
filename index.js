f303988e4d217b741d374520c477f6fe532ff005
Litt1eShineStar <60056665+Litt1eShineStar@users.noreply.github.com>
2023-07-06 19:02:59 +0800

    feat: add paddle element

--- index.js
+++ index.js
@@ -6,6 +6,33 @@ let y = canvas.height - 30 // start point y
 let dx = 2
 let dy = -2
 const ballRadius = 10
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let leftPressed = false;
+let rightPressed = false;
+let interval;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    const { key } = e;
+    if (key === 'ArrowRight') {
+        rightPressed = true
+    } else if (key === 'ArrowLeft') {
+        leftPressed = true
+    }
+}
+
+function keyUpHandler(e) {
+    const { key } = e;
+    if (key === 'ArrowRight') {
+        rightPressed = false
+    } else if (key === 'ArrowLeft') {
+        leftPressed = false
+    }
+}
 
 // define a ball
 function drawBall() {
@@ -17,9 +44,6 @@ function drawBall() {
 }
 
 // define a paddle to hit the ball
-const paddleHeight = 10;
-const paddleWidth = 75;
-let paddleX = (canvas.width - paddleWidth) / 2;
 function drawPaddle() {
     ctx.beginPath()
     ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight)
@@ -28,19 +52,60 @@ function drawPaddle() {
     ctx.closePath()
 }
 
+// start
+function start() {
+    interval = setInterval(draw, 10)
+}
+
+// reset
+function reset() {
+    console.log('game over')
+    // document.location.reload()
+    x = canvas.width / 2  // start point x
+    y = canvas.height - 30 // start point y
+    dx = 2
+    dy = -2
+    paddleX = (canvas.width - paddleWidth) / 2;
+    leftPressed = false;
+    rightPressed = false;
+    clearInterval(interval)
+    interval = null;
+}
+
 // draw code
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall()
     drawPaddle()
-    if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+    // ball moving
+    if (y + dy < ballRadius) {
         dy = -dy
+    } else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy
+        } else {
+            reset()
+        }
     }
     if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
         dx = -dx
     }
     x += dx
     y += dy
+    // paddle moving
+    if (leftPressed && rightPressed) {
+        // stop moving
+    } else if (rightPressed) {
+        paddleX += 5;
+        if (paddleX + paddleWidth > canvas.width) {
+            paddleX = canvas.width - paddleWidth;
+        }
+    } else if (leftPressed) {
+        paddleX -= 5;
+        if (paddleX < 0) {
+            paddleX = 0
+        }
+    }
 }
 
-setInterval(draw, 10)
+start()
